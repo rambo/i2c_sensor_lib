55a15d538315f1298f1bc4d742a1a576d8d38ab3
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-01-21 10:46:38 +0200

    rename & fix files for arduino 1.0

---
+++ avgbuffer.h
@@ -0,0 +1,7 @@
+#ifndef avgbuffer_h
+#define avgbuffer_h
+#include <WProgram.h>
+#include "filter.h"
+
+#endif
+// *********** END OF CODE **********
---
+++ filter.h
@@ -0,0 +1,7 @@
+#ifndef filter_h
+#define filter_h
+#include <WProgram.h>
+
+
+#endif
+// *********** END OF CODE **********
